5d69f804f370b88155651aaacc1947bdc8e27c86
Carlito Algarin <carlito1113@yahoo.com>
2020-12-10 02:56:30 -0500

    added in javascript for the button click functions and creating todos, adjusted styling. switched div classes causing the saved todo boxes to be improper size

---
+++ app.js
@@ -0,0 +1,33 @@
+// SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// EVENT LISTENERS
+todoButton.addEventListener('click', addTodo);
+
+// FUNCTIONS
+function addTodo(event) {
+        // PREVENT FORM FROM SUBMITTING
+    event.preventDefault();
+    // TODO DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // CREATE LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = "<i class='fas fa-check'></i>";
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // CHECK TRASH BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv);
+}
